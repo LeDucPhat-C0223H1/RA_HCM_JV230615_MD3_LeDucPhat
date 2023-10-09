087633690a3a21444a839675fe839a1832e0c709
LeDucPhat-C0223H1 <leducphat2808@gmail.com>
2023-10-09 11:02:31 +0700

    nop phan bai tap session14

--- src/Practice/BT1/SapXepTangDan.java
+++ src/Practice/BT1/SapXepTangDan.java
@@ -1,20 +1,43 @@
 package Practice.BT1;
 
 import DemoSortingAlgorithms.BubbleSort;
+import DemoSortingAlgorithms.InsertionSort;
+import DemoSortingAlgorithms.SelectionSort;
 
 import java.util.Arrays;
 
 public class SapXepTangDan {
     public static void main(String[] args) {
-        System.out.println("Mảng gốc ban đầu:");
+        //Sắp xếp tăng dần kiểu nổi bọt
+        System.out.println("Mảng thứ 1 ban đầu:");
         Integer[] array1 = getArr();
         System.out.println(Arrays.toString(array1));
-        System.out.println();
-
-        //Sắp xếp tăng dần kiểu nổi bọt
         System.out.println("Sắp xếp tăng dần kiểu nổi bọt");
         BubbleSort.bubbleSort(array1);
-        System.out.printf(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array1));
+        System.out.println();
+        /*
+        *
+        * */
+        //Sắp xếp tăng dần kiểu chọn
+        System.out.println("Mảng thứ 2 ban đầu:");
+        Integer[] array2 = getArr();
+        System.out.println(Arrays.toString(array2));
+        System.out.println("Sắp xếp tăng dần kiểu chọn");
+        SelectionSort.selectionSort(array2);
+        System.out.println(Arrays.toString(array2));
+        System.out.println();
+        /*
+         *
+         * */
+        //Sắp xếp tăng dần kiểu chọn
+        System.out.println("Mảng thứ 3 ban đầu:");
+        Integer[] array3 = getArr();
+        System.out.println(Arrays.toString(array3));
+        System.out.println("Sắp xếp tăng dần kiểu chèn");
+        InsertionSort.intertionSort(array3);
+        System.out.println(Arrays.toString(array3));
+        System.out.println();
     }
 
     private static Integer[] getArr() {
